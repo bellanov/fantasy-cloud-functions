3226d1366c036c7bfe0689ebde6fde8a0e8cb287
Bellanov <bellanovapilli@gmail.com>
2025-08-13 16:11:49 -0400

    Implemented Event Odds cloud function

--- functions/src/eventOdds.ts
+++ functions/src/eventOdds.ts
@@ -16,22 +16,46 @@ export const getEventOdds = onRequest(async (request, response) => {
   try {
     logger.info("Retrieving Event Odds Data");
 
+    // Get eventId to query
+    const eventId = request.query.eventId as string;
+
+    // Get format to return
+    const format = (request.query.format as string) || "american";
+
+    logger.info(`Event ID set to: ${eventId}`);
+    logger.info(`Format set to: ${format}`);
+
+    if (!eventId) {
+      logger.warn("No eventId provided");
+      response.status(400).send("Bad Request: eventId is required");
+      return;
+    }
+
     // Build and execute the query
-    db.collection("sports")
+    db.collectionGroup(format)
+      .where("id", "==", eventId)
       .get()
       .then((snapshot) => {
         if (!snapshot.empty) {
-          const eventOddsData = snapshot.docs.map((doc) => doc.data());
-          response.json({data: eventOddsData});
+          const eventsData = snapshot.docs.map((doc) => doc.data());
+          response.json({
+            data: eventsData,
+          });
         } else {
-          logger.warn("No such document!");
+          logger.warn("No such documents!");
+          response
+            .status(400)
+            .send(
+              "Bad Request: Odds Format (?format) is possibly invalid.",
+            );
+          return;
         }
       })
       .catch((error) => {
         logger.error("Error getting document:", error);
       });
   } catch (error) {
-    logger.error("Error creating user:", error);
+    logger.error("Error retrieving odds:", error);
     response.status(500).send("Internal Server Error");
   }
 });
